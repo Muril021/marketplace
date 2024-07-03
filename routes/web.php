9fde0d663c7d840204e615d4fe1024d3e0ad0693
Murilo Nascimento <murilojesus659@gmail.com>
2024-07-03 16:18:25 -0300

    feature: routes to admin and vendor dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,50 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\VendorController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
+Route::get('/dashboard',function () {
+  return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+Route::prefix('/profile')
+->group(function () {
+  Route::get('', [
+    ProfileController::class,
+    'edit'
+  ])->name('profile.edit');
+
+  Route::patch('', [
+    ProfileController::class,
+    'update'
+  ])->name('profile.update');
+
+  Route::delete('', [
+    ProfileController::class,
+    'destroy'
+  ])->name('profile.destroy');
+})->middleware('auth');
 
 require __DIR__.'/auth.php';
+
+Route::prefix('admin')
+->group(function () {
+  Route::get('/dashboard', [
+    AdminController::class,
+    'dashboard'
+  ])->name('admin.dashboard');
+})->middleware('auth');
+
+Route::prefix('vendor')
+->group(function () {
+  Route::get('/dashboard', [
+    VendorController::class,
+    'dashboard'
+  ])->name('vendor.dashboard');
+})->middleware('auth');
