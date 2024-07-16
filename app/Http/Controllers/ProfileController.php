542fddcd141a6fac1f5f74fb809d09a3c4f7f354
Murilo <murilojesus659@gmail.com>
2024-07-16 14:42:11 -0300

    feature: profile page and data update

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,59 +2,36 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\ProfileUpdateRequest;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Redirect;
-use Illuminate\View\View;
 
 class ProfileController extends Controller
 {
-    /**
-     * Display the user's profile form.
-     */
-    public function edit(Request $request): View
-    {
-        return view('profile.edit', [
-            'user' => $request->user(),
-        ]);
-    }
+  public function index()
+  {
+    return view('admin.profile.index');
+  }
 
-    /**
-     * Update the user's profile information.
-     */
-    public function update(ProfileUpdateRequest $request): RedirectResponse
-    {
-        $request->user()->fill($request->validated());
+  public function update(Request $request)
+  {
+    // dd($request->all());
+    $request->validate([
+      'name' => ['required', 'max:100'],
+      'email' => [
+        'required',
+        'email',
+        'unique:users,email,'.Auth::user()->id
+      ]
+    ]);
 
-        if ($request->user()->isDirty('email')) {
-            $request->user()->email_verified_at = null;
-        }
+    $user = Auth::user();
+    $user->name = $request->name;
+    $user->email = $request->email;
+    $user->save();
 
-        $request->user()->save();
-
-        return Redirect::route('profile.edit')->with('status', 'profile-updated');
-    }
-
-    /**
-     * Delete the user's account.
-     */
-    public function destroy(Request $request): RedirectResponse
-    {
-        $request->validateWithBag('userDeletion', [
-            'password' => ['required', 'current_password'],
-        ]);
-
-        $user = $request->user();
-
-        Auth::logout();
-
-        $user->delete();
-
-        $request->session()->invalidate();
-        $request->session()->regenerateToken();
-
-        return Redirect::to('/');
-    }
+    return redirect()->back()->with(
+      'success',
+      'Dados atualizados com sucesso.'
+    );
+  }
 }
--- routes/web/admin.php
+++ routes/web/admin.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'admin'])
@@ -10,4 +11,14 @@ Route::middleware(['auth', 'admin'])
     AdminController::class,
     'dashboard'
   ])->name('admin.dashboard');
+
+  Route::get('/profile', [
+    ProfileController::class,
+    'index'
+  ])->name('admin.profile');
+
+  Route::post('/profile/update', [
+    ProfileController::class,
+    'update'
+  ])->name('admin.profile.update');
 });
