09c7c67e951ccb73f91ab63ba845ced3b38aef70
Murilo <murilojesus659@gmail.com>
2024-07-18 15:23:22 -0300

    feature: implementing notification bubbles

--- app/Http/Controllers/AdminProfileController.php
+++ app/Http/Controllers/AdminProfileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 use File;
 
 class AdminProfileController extends Controller
@@ -16,58 +17,72 @@ class AdminProfileController extends Controller
   public function update(Request $request)
   {
     // dd($request->all());
-    $request->validate([
-      'name' => ['required', 'max:100'],
-      'email' => [
-        'required',
-        'email',
-        'unique:users,email,'.Auth::user()->id
-      ],
-      'image' => ['image', 'max:2048']
-    ]);
+    try {
+      $request->validate([
+        'name' => ['required', 'max:100'],
+        'email' => [
+          'required',
+          'email',
+          'unique:users,email,'.Auth::user()->id
+        ],
+        'image' => ['image', 'max:2048']
+      ]);
 
-    $user = Auth::user();
+      $user = Auth::user();
 
-    if ($request->hasFile('image')) {
-      // verifica se a imagem existe e apaga
-      if (File::exists(public_path($user->image))) {
-        File::delete(public_path($user->image));
+      if ($request->hasFile('image')) {
+        // verifica se a imagem existe e apaga
+        if (File::exists(public_path($user->image))) {
+          File::delete(public_path($user->image));
+        }
+
+        $image = $request->image;
+        $imageName = rand().$image->getClientOriginalName();
+        $image->move(public_path('uploads'), $imageName);
+
+        $path = "/uploads/".$imageName;
+
+        $user->image = $path;
       }
 
-      $image = $request->image;
-      $imageName = rand().$image->getClientOriginalName();
-      $image->move(public_path('uploads'), $imageName);
+      $user->name = $request->name;
+      $user->email = $request->email;
+      $user->save();
 
-      $path = "/uploads/".$imageName;
-
-      $user->image = $path;
+      toastr()->success('Dados atualizados com sucesso.');
+    } catch (ValidationException $error) {
+      foreach ($error->errors() as $messages) {
+        foreach ($messages as $message) {
+          toastr()->error($message);
+        }
+      }
     }
 
-    $user->name = $request->name;
-    $user->email = $request->email;
-    $user->save();
-
-    return redirect()->back()->with(
-      'success',
-      'Dados atualizados com sucesso.'
-    );
+    return redirect()->back();
   }
 
   public function updatePassword(Request $request)
   {
     // dd($request->all());
-    $request->validate([
-      'current_password' => ['required', 'current_password'],
-      'password' => ['required', 'confirmed', 'min:8']
-    ]);
+    try {
+      $request->validate([
+        'current_password' => ['required', 'current_password'],
+        'password' => ['required', 'confirmed', 'min:8']
+      ]);
 
-    $request->user()->update([
-      'password' => bcrypt($request->password)
-    ]);
+      $request->user()->update([
+        'password' => bcrypt($request->password)
+      ]);
 
-    return redirect()->back()->with(
-      'success',
-      'Senha alterada com sucesso.'
-    );
+      toastr()->success('Senha alterada com sucesso.');
+    } catch (ValidationException $error) {
+      foreach ($error->errors() as $messages) {
+        foreach ($messages as $message) {
+          toastr()->error($message);
+        }
+      }
+    }
+
+    return redirect()->back();
   }
 }
